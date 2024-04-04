971750eaf70d771b24a97d29357fbeb899755391
Jeff Eaton <jeff@eaton.fyi>
2024-04-04 17:55:24 -0500

    Initial commit, just faffing about.

---
+++ README.md
@@ -0,0 +1,3 @@
+# SubStats
+
+A quick and dirty tool that pulls posts from a given subreddit for a time period, and generates a CSV file with stats about the content.
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import 'dotenv/config'
+import jetpack from '@eatonfyi/fs-jetpack';
+import { Tsv } from '@eatonfyi/serializers';
+import { createRedditClient, SortingMethod, SubredditData } from "reddit-explorer"
+import { z } from 'zod';
+
+if (!!process.env.REDDIT_SUB) {
+  console.error('Set the REDDIT_SUB env variable to the name of the subreddit.');
+  process.exit();
+}
+
+jetpack.setSerializer('.tsv', Tsv);
+const dir = jetpack.dir('output');
+
+const PostSchema = z.object({
+  created: z.number().transform(c => new Date(c * 1000).toISOString().replace('.000Z', '').replace('T', ' ')),
+  author: z.string().optional().nullable().default(''),
+  author_flair_text: z.string().optional().nullable().default(''),
+  link_flair_text: z.coerce.string().optional().default(''),
+  title: z.string(),
+  num_comments: z.number().default(0),
+  edited: z.coerce.boolean().default(false),
+  score: z.coerce.number().default(0),
+  mod_reports: z.array(z.unknown()).transform(r => r.length ?? 0),
+  user_reports: z.array(z.unknown()).transform(r => r.length ?? 0),
+  selftext: z.coerce.string().optional().transform(t => t?.replaceAll(/[\r\n]+/g, '\\n')),
+});
+
+// results.value.data.children[].data
+const ResponseSchema = z.object({
+  value: z.object({
+    data: z.object({
+      children: z.array(z.object({
+        data: PostSchema
+      }))
+    })
+  })
+});
+type Post = z.infer<typeof PostSchema>;
+
+
+const reddit = createRedditClient({
+  clientId: process.env.REDDIT_CLIENT ?? '',
+  secret: process.env.REDDIT_SECRET ?? '',
+})
+reddit.getAccessToken();
+
+const subIterator = reddit.getSubredditIterator({
+  name: process.env.REDDIT_SUB ?? '',
+  sortMethod: SortingMethod.New,
+  limit: 50,
+});
+
+const output: Post[] = [];
+
+let i = 0;
+while (i++ < 11) {
+  const results = await subIterator.next();
+
+  const parsed = ResponseSchema.parse(results);
+  const posts = parsed.value.data.children.map(c => c.data);
+
+  output.push(...posts);
+  dir.write('posts.tsv', output);
+}
